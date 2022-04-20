8deadd5c3fad303cbd3288d4df4da40e06bb0b34
Yuan-Mao <ymchueh@gmail.com>
2022-04-19 18:53:23 -0700

    Use bsg_edge_detect instead

--- ethernet_controller/rtl/template/interrupt_control_unit.sv
+++ ethernet_controller/rtl/template/interrupt_control_unit.sv
@@ -18,7 +18,6 @@ module interrupt_control_unit (
   , output logic    tx_interrupt_pending_o
 );
 
-  logic tx_ready_prev_r;
   logic rx_interrupt_enable_r;
   logic tx_interrupt_enable_r;
 
@@ -40,21 +39,17 @@ module interrupt_control_unit (
        ,.data_o(tx_interrupt_enable_r)
     );
 
-
-
-  // reset value is 1 because the init value of packet_req_i is 1
-  // and we use 0 -> 1 transition of tx_ready_prev_r
-  // as the interrupt setting event
-  bsg_dff_reset #(.width_p(1), .reset_val_p(1))
-    tx_ready_prev_reg (
+  // Set tx interupt when packet_req_i goes from 0 -> 1
+  // Since the init value of packet_req_i is 1, and
+  // bsg_edge_detect can only set init value to 0,
+  // ~packet_req_i and falling edge are used.
+  bsg_edge_detect #(.falling_not_rising_p(1))
+    edge_detect (
         .clk_i(clk_i)
        ,.reset_i(reset_i)
-       ,.data_i(packet_req_i)
-       ,.data_o(tx_ready_prev_r)
+       ,.sig_i(~packet_req_i)
+       ,.detect_o(tx_interrupt_set_li)
     );
-
-  // set tx interupt when packet_req_i goes from 0 -> 1
-  wire tx_interrupt_set_li = (tx_ready_prev_r == 0) & packet_req_i;
 
   logic tx_interrupt_pending_r_lo;
 
@@ -72,6 +67,7 @@ module interrupt_control_unit (
   // remove one received packet in the RX buffer. If after that the buffer happens
   // to become empty, the RX pending bit will goes to 0, otherwise it keeps being
   // 1.
+  // TODO: enable bit should not mask the pending bit
   assign rx_interrupt_pending_o = packet_avail_i & rx_interrupt_enable_r;
   assign tx_interrupt_pending_o = tx_interrupt_pending_r_lo & tx_interrupt_enable_r;
 
