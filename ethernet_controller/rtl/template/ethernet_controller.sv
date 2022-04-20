ffd3e9de3a7abd85e778932de9cc0abca044cbf8
Yuan-Mao <ymchueh@gmail.com>
2022-04-19 18:30:01 -0700

    Remove mac_with_buffer

--- ethernet_controller/rtl/template/ethernet_controller.sv
+++ ethernet_controller/rtl/template/ethernet_controller.sv
@@ -72,6 +72,22 @@ module ethernet_controller #
   logic tx_interrupt_enable_lo, tx_interrupt_enable_v_lo;
   logic rx_interrupt_pending_lo, tx_interrupt_pending_lo;
 
+
+  logic [data_width_p-1:0]   tx_axis_tdata_lo;
+  logic [data_width_p/8-1:0] tx_axis_tkeep_lo;
+  logic                      tx_axis_tvalid_lo;
+  logic                      tx_axis_tlast_lo;
+  logic                      tx_axis_tready_li;
+  logic                      tx_axis_tuser_lo;
+
+  logic [data_width_p-1:0]   rx_axis_tdata_li;
+  logic [data_width_p/8-1:0] rx_axis_tkeep_li;
+  logic                      rx_axis_tvalid_li;
+  logic                      rx_axis_tready_lo;
+  logic                      rx_axis_tlast_li;
+  logic                      rx_axis_tuser_li;
+
+
   wire [15:0] debug_info_li = {
     tx_error_underflow_lo
    ,tx_fifo_overflow_lo
@@ -132,66 +148,100 @@ module ethernet_controller #
    ,.io_decode_error_o(io_decode_error_lo)
   );
 
-  // synopsys translate_off
-  always_ff @(negedge clk_i) begin
-    if(~reset_i) begin
-      assert(io_decode_error_lo == 0) else
-        $error("ethernet_controller.sv: io decode error\n");
-    end
-  end
-  // synopsys translate_on
 
+  ethernet_sender #(
+       .data_width_p(data_width_p)
+      ,.eth_mtu_p(eth_mtu_lp))
+   sender (
+       .clk_i(clk_i)
+      ,.reset_i(reset_i)
 
-  mac_with_buffer #(
-    .eth_mtu_p(eth_mtu_lp)
-   ,.data_width_p(data_width_p)
-  ) eth (
-    .clk_i(clk_i)
-   ,.reset_i(reset_i)
-   ,.clk250_i(clk250_i)
-   ,.reset_clk250_i(reset_clk250_i)
-   ,.reset_clk125_o(reset_clk125_o)
+      ,.packet_send_i(packet_send_lo)
+      ,.packet_req_o(packet_req_lo)
+      ,.packet_wsize_valid_i(packet_wsize_valid_lo)
+      ,.packet_wsize_i(packet_wsize_lo)
+      ,.packet_wvalid_i(packet_wvalid_lo)
+      ,.packet_waddr_i(packet_waddr_lo)
+      ,.packet_wdata_i(packet_wdata_lo)
+      ,.packet_wdata_size_i(packet_wdata_size_lo)
 
-   ,.packet_send_i(packet_send_lo)
-   ,.packet_req_o(packet_req_lo)
-   ,.packet_wsize_valid_i(packet_wsize_valid_lo)
-   ,.packet_wsize_i(packet_wsize_lo)
-   ,.packet_wvalid_i(packet_wvalid_lo)
-   ,.packet_waddr_i(packet_waddr_lo)
-   ,.packet_wdata_i(packet_wdata_lo)
-   ,.packet_wdata_size_i(packet_wdata_size_lo)
+      ,.tx_axis_tdata_o(tx_axis_tdata_lo)
+      ,.tx_axis_tkeep_o(tx_axis_tkeep_lo)
+      ,.tx_axis_tvalid_o(tx_axis_tvalid_lo)
+      ,.tx_axis_tlast_o(tx_axis_tlast_lo)
+      ,.tx_axis_tready_i(tx_axis_tready_li)
+      ,.tx_axis_tuser_o(tx_axis_tuser_lo)
 
-   ,.packet_ack_i(packet_ack_lo)
-   ,.packet_avail_o(packet_avail_lo)
-   ,.packet_rvalid_i(packet_rvalid_lo)
-   ,.packet_raddr_i(packet_raddr_lo)
-   ,.packet_rdata_size_i(packet_rdata_size_lo)
-   ,.packet_rdata_o(packet_rdata_lo)
-   ,.packet_rsize_o(packet_rsize_lo)
+      ,.send_count_o(/* UNUSED */)
+  );
 
-   ,.rgmii_rx_clk_i(rgmii_rx_clk_i)
-   ,.rgmii_rxd_i(rgmii_rxd_i)
-   ,.rgmii_rx_ctl_i(rgmii_rx_ctl_i)
-   ,.rgmii_tx_clk_o(rgmii_tx_clk_o)
-   ,.rgmii_txd_o(rgmii_txd_o)
-   ,.rgmii_tx_ctl_o(rgmii_tx_ctl_o)
+  ethernet_receiver #(
+      .data_width_p(data_width_p)
+     ,.eth_mtu_p(eth_mtu_lp))
+   receiver (
+      .clk_i(clk_i)
+     ,.reset_i(reset_i)
 
-   ,.tx_error_underflow_o(tx_error_underflow_lo)
-   ,.tx_fifo_overflow_o(tx_fifo_overflow_lo)
-   ,.tx_fifo_bad_frame_o(tx_fifo_bad_frame_lo)
-   ,.tx_fifo_good_frame_o(tx_fifo_good_frame_lo)
-   ,.rx_error_bad_frame_o(rx_error_bad_frame_lo)
-   ,.rx_error_bad_fcs_o(rx_error_bad_fcs_lo)
-   ,.rx_fifo_overflow_o(rx_fifo_overflow_lo)
-   ,.rx_fifo_bad_frame_o(rx_fifo_bad_frame_lo)
-   ,.rx_fifo_good_frame_o(rx_fifo_good_frame_lo)
+     ,.packet_ack_i(packet_ack_lo)
+     ,.packet_avail_o(packet_avail_lo)
+     ,.packet_rvalid_i(packet_rvalid_lo)
+     ,.packet_raddr_i(packet_raddr_lo)
+     ,.packet_rdata_size_i(packet_rdata_size_lo)
+     ,.packet_rdata_o(packet_rdata_lo)
+     ,.packet_rsize_o(packet_rsize_lo)
 
-   ,.send_count_o(/* UNUSED */)
-   ,.receive_count_o(/* UNUSED */)
+     ,.rx_axis_tdata_i(rx_axis_tdata_li)
+     ,.rx_axis_tkeep_i(rx_axis_tkeep_li)
+     ,.rx_axis_tvalid_i(rx_axis_tvalid_li)
+     ,.rx_axis_tready_o(rx_axis_tready_lo)
+     ,.rx_axis_tlast_i(rx_axis_tlast_li)
+     ,.rx_axis_tuser_i(rx_axis_tuser_li)
 
-   ,.speed_o(speed_lo)
-   );
+     ,.receive_count_o(/* UNUSED */)
+  );
 
+  eth_mac_1g_rgmii_fifo #(.AXIS_DATA_WIDTH(data_width_p))
+      mac (
+      .clk250(clk250_i)
+     ,.clk250_rst(reset_clk250_i)
+     ,.gtx_rst(reset_clk125_o)
+     ,.logic_clk(clk_i)
+     ,.logic_rst(reset_i)
+
+     ,.tx_axis_tdata(tx_axis_tdata_lo)
+     ,.tx_axis_tkeep(tx_axis_tkeep_lo)
+     ,.tx_axis_tvalid(tx_axis_tvalid_lo)
+     ,.tx_axis_tready(tx_axis_tready_li)
+     ,.tx_axis_tlast(tx_axis_tlast_lo)
+     ,.tx_axis_tuser(tx_axis_tuser_lo)
+
+     ,.rx_axis_tdata(rx_axis_tdata_li)
+     ,.rx_axis_tkeep(rx_axis_tkeep_li)
+     ,.rx_axis_tvalid(rx_axis_tvalid_li)
+     ,.rx_axis_tready(rx_axis_tready_lo)
+     ,.rx_axis_tlast(rx_axis_tlast_li)
+     ,.rx_axis_tuser(rx_axis_tuser_li)
+
+     ,.rgmii_rx_clk(rgmii_rx_clk_i)
+     ,.rgmii_rxd(rgmii_rxd_i)
+     ,.rgmii_rx_ctl(rgmii_rx_ctl_i)
+     ,.rgmii_tx_clk(rgmii_tx_clk_o)
+     ,.rgmii_txd(rgmii_txd_o)
+     ,.rgmii_tx_ctl(rgmii_tx_ctl_o)
+
+     ,.tx_error_underflow(tx_error_underflow_lo)
+     ,.tx_fifo_overflow(tx_fifo_overflow_lo)
+     ,.tx_fifo_bad_frame(tx_fifo_bad_frame_lo)
+     ,.tx_fifo_good_frame(tx_fifo_good_frame_lo)
+     ,.rx_error_bad_frame(rx_error_bad_frame_lo)
+     ,.rx_error_bad_fcs(rx_error_bad_fcs_lo)
+     ,.rx_fifo_overflow(rx_fifo_overflow_lo)
+     ,.rx_fifo_bad_frame(rx_fifo_bad_frame_lo)
+     ,.rx_fifo_good_frame(rx_fifo_good_frame_lo)
+     ,.speed(speed_lo)
+
+     ,.ifg_delay(8'd12)
+  );
 
   interrupt_control_unit interrupt_control_unit (
     .clk_i(clk_i)
@@ -214,5 +264,15 @@ module ethernet_controller #
 
   assign rx_interrupt_pending_o = rx_interrupt_pending_lo;
   assign tx_interrupt_pending_o = tx_interrupt_pending_lo;
+
+  // synopsys translate_off
+  always_ff @(negedge clk_i) begin
+    if(~reset_i) begin
+      assert(io_decode_error_lo == 0) else
+        $error("ethernet_controller.sv: io decode error\n");
+    end
+  end
+  // synopsys translate_on
+
 
 endmodule
